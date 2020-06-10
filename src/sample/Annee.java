0b1b93655e479fdff5a9c65c1488d99ee02ec776
sada <armand.diedhiou@u-psud.fr>
2020-06-10 17:02:21 +0200

    projet

---
+++ src/sample/Annee.java
@@ -0,0 +1,7 @@
+package sample;
+
+import java.util.HashMap;
+
+public class Annee extends HashMap {
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+    	Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+
+        primaryStage.setTitle("Projet IHM 2020");
+        primaryStage.setScene(new Scene(root, 695, 431));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
